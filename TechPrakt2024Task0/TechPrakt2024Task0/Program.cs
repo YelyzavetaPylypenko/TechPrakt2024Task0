2e031e7fe296f7bcacef4de6670a8a30b338c0ad
Yelyzaveta Pylypenko <ipz221_pyev@student.ztu.edu.ua>
2024-02-10 22:39:42 +0200

    add .gitignore, README.md, TechPrakt2024Task0/

---
+++ TechPrakt2024Task0/TechPrakt2024Task0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Transactions;
+
+namespace TechPrakt2024Task0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding =Encoding.UTF8;
+            Console.Write("Enter the first number: ");
+            if  (double.TryParse(Console.ReadLine(), out double number1))
+            {
+                Console.Write("Ente the second number: ");
+               if(double.TryParse(Console.ReadLine(), out double number2))
+               {
+                    double add = number1 + number2;
+                    Console.WriteLine($"Sum of numbers {number1} and {number2} = {add}");
+               }
+                else
+                {
+                    Console.WriteLine("Incorrect second number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect first number.");
+            }
+        }
+    }
+}
